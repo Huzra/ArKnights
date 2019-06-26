2039ad4ad4e5fa74720f8c797606295108f120ad
TakamiSama <291781096@qq.com>
2019-06-27 00:18:15 +0800

    db
    db

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,5 +1,8 @@
 // pages/mine/mine.js
 var app = getApp()
+const db = wx.cloud.database({
+  env: 'nmsl-fcwyb'
+})
 Page({
 
   /**
@@ -84,6 +87,29 @@ Page({
         userInfo: e.detail.userInfo,
         hasUserInfo: true
       })
+      if(db.collection('user').where({
+        name: app.globalData.userInfo.nikeName
+      }).get()!=null)
+      {
+        db.collection('user').add({
+          // data 字段表示需新增的 JSON 数据
+          data: {
+            // _id: 'todo-identifiant-aleatoire', // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
+            name: app.globalData.userInfo.nikeName,
+            follow_num: "",
+            followed_num:"",
+            sound_num:"",
+            intro:"",
+            follow:[],
+            sound:[],
+            img: app.globalData.userInfo.avatarUrl
+          },
+          success: function (res) {
+            // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+            console.log(res)
+          }
+        })
+      }
     }
   },
 
